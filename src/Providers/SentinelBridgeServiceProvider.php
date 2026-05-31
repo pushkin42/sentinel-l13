40a8ce064fdd5f8887080dab11540309110f25eb
VIACHESLAV KHOMENKO <pushkin42@users.noreply.github.com>
2026-05-31 17:32:41 +0300

    Add SentinelAuthServiceProvider for Sentinel integration

---
+++ src/Providers/SentinelBridgeServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Contracts\Hashing\Hasher;
+use Cartalyst\Sentinel\Laravel\Auth\SentinelUserProvider;
+
+class SentinelAuthServiceProvider extends ServiceProvider
+{
+    public function boot(): void
+    {
+        Auth::provider('sentinel', function ($app, array $config) {
+            return new SentinelUserProvider(
+                $app['hash'],
+                $config['model']
+            );
+        });
+      
+        Auth::extend('sentinel-session', function ($app, $name, $config) {
+            $provider = Auth::createUserProvider($config['provider']);
+            return new SentinelSessionGuard(
+                $name,
+                $app['sentinel'],
+                $provider,
+                $app['session.store'],
+                $app['request']
+            );
+        });
+    }
+}
